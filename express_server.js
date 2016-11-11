17dfe596274277dac92dcd96cbd5625108325b80
jspuravida <julian.salari@gmail.com>
2016-11-11 04:12:48 +0000

    Added headers, registration page, registration handler, cookies, encryption

--- express_server.js
+++ express_server.js
@@ -1,19 +1,63 @@
-var express = require("express");
-var cookieParser = require('cookie-parser')
-
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
-
+const express = require("express");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser())
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
 
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
+app.set('trust proxy', 1);
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/register", (req, res) => {
+  res.render("registration");
+});
+
+const users = {
+
+ "2322323": {id: "l3l3l3", email: "tom@hotmail.com", password: "beoeoeoe"},
+ "666653": {id: "23232", email: "ted@hotmail.com", password: "beoeoeoe"},
+
+};
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if(email === "" || password === "" ) {
+    res.send("Error: 400");
+    return;
+  } else {
+    var foundEmail = false;
+    Object.keys(users).forEach(function(key) {
+      if (users[key].email == email) {
+        foundEmail = true;
+      }
+    });
+    if (foundEmail) {
+      res.send("Error: 400");
+      return;
+    }
+  }
+  const userRandomID = generateRandomString();
+  const saltRounds = 10;
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    const newUser = { id: userRandomID, email: email, password: hash };
+    users[userRandomID] = newUser;
+    res.cookie("key", userRandomID);
+    console.log(users);
+    res.redirect("/");
+  });
+});
 
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.username);
@@ -40,8 +84,6 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 // Grab the short url and add it to the urlDatabase
-
-
 
 
 app.get("/urls", (req, res) => {
@@ -83,15 +125,14 @@ app.listen(PORT, () => {
 });
 
 function generateRandomString() {
-
   var list = "abcdefghijklmnopqrstuvwxyz0123456789"; //list of characters to choose from
   var urlShortened = ""; //stores the new url
-
   for (var i = 0; i < 6; i++) { //loops until it hits the set max length
     urlShortened += list.charAt(Math.floor(Math.random() * list.length)); //randomly outputs a character from the list and stores in urlShortened
   }
   return urlShortened;
 }
+
 
 
 
